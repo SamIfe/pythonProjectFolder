af0ae1433a911e1981c6cde5f5f281d6164e53e0
Ifeoluwa <os.ifeoluwa@gmail.com>
2024-09-30 15:22:18 +0100

    Program that generates two integers under 100 using random number generator

--- phase_gate1/integer_number_generator.py
+++ phase_gate1/integer_number_generator.py
@@ -1,6 +1,13 @@
-number1 = 0
-if number1 <= 100:
-	number1 = int(input("Enter the first integer: "))
-else:
-	print("Enter another number: ")
-number2 = int(input("Enter the first integer: "))
+import random
+
+number_one = random.randint(0, 99)
+number_two = random.randint(0, 99)
+
+print(f"What is the expected sum of  {number_one} and {number_two}?: ")
+
+user_response = int(input("Enter your answer: "))
+	
+correct_response = number_one + number_two
+correct_answer = (user_response == 	correct_response)
+
+print("Your answer is:", correct_answer)
