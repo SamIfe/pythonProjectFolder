c821360aae89a399b2c79ed00a814299665a5ae9
Ifeoluwa <os.ifeoluwa@gmail.com>
2024-10-04 16:13:54 +0100

    Program that find all such numbers between 1000 and 3000 and print the even number

---
+++ phase_gate2/even_digit_number.py
@@ -0,0 +1,11 @@
+def even_digit_number(number):
+  list = 3001
+  for number in range (1000, list):
+      if number % 2 == 0:
+        return number;
+
+
+print(even_digit_number(3001))
+
+      
+	
