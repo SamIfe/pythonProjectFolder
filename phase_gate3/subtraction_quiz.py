6e5c60f8d0eb3a3b8beb807b0b57c3e392576247
Ifeoluwa <os.ifeoluwa@gmail.com>
2024-10-07 17:14:34 +0100

    Question 1 of 2 of the second round of phasegate on 10/7/2024

---
+++ phase_gate3/subtraction_quiz.py
@@ -0,0 +1,53 @@
+import random
+welcome = """
++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+	WELCOME TO THE SUBSTRACTION QUIZ!
+
+
+	>>. Ensure that you attempt all questions
+
+	>>. You will be given 10 random subtraction problems.
+
+	>>. You can attempt each problem up to 10 times.
+
+			Goodluck!!
++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+"""
+print (welcome)
+total_score = 0
+total_question = 10
+
+for exam in range(total_question):
+  first_rand_number = random.randint (1, 100)
+  second_rand_number = random.randint (1, first_rand_number)
+
+  correct_answer = first_rand_number - second_rand_number
+  attempt_question = 0 
+  correct_attempt = False
+
+  print(f" Question {exam + 1} What is {first_rand_number} - {second_rand_number}: ")
+
+while attempt_question < 10 and not correct_attempt:
+  candidate_answer = int(input(f"Attempt question {attempt_question + 1} your answer: "))
+
+try:
+  if candidate_answer == correct_answer:
+    print("Correct")
+    total_score += 1
+    correct_attempt = True
+  else:
+    print("Incorrect. Try again.")
+except ValueError:
+    print("Please enter a valid integer.")
+        
+    attempt_question += 1
+if not correct_attempt:
+    print(f"The correct answer was {correct_answer}.\n")
+
+
+
+    
+
+
+  
+  
