00bd4b01c10f26fce3650afcd3e92ba49c3e2949
Ifeoluwa <os.ifeoluwa@gmail.com>
2024-10-07 21:08:25 +0100

    Updated class task

--- phase_gate3/subtraction_quiz.py
+++ phase_gate3/subtraction_quiz.py
@@ -25,25 +25,25 @@ for exam in range(total_question):
   attempt_question = 0 
   correct_attempt = False
 
-  print(f" Question {exam + 1} What is {first_rand_number} - {second_rand_number}: ")
+  print(f" Question {exam + 1}: What is {first_rand_number} - {second_rand_number}? ")
 
-while attempt_question < 10 and not correct_attempt:
-  candidate_answer = int(input(f"Attempt question {attempt_question + 1} your answer: "))
+  while attempt_question < 10 and not correct_attempt:
+    candidate_answer = int(input(f"Attempt question {attempt_question + 1}: your answer: "))
 
-try:
-  if candidate_answer == correct_answer:
-    print("Correct")
-    total_score += 1
-    correct_attempt = True
-  else:
-    print("Incorrect. Try again.")
-except ValueError:
-    print("Please enter a valid integer.")
+    try:
+      if candidate_answer == correct_answer:
+        print("Correct\n")
+        total_score += 1
+        correct_attempt = True
+      else:
+        print("Incorrect. Try again.")
+    except ValueError:
+      print("Please enter a valid integer.")
         
     attempt_question += 1
-if not correct_attempt:
+  if not correct_attempt:
     print(f"The correct answer was {correct_answer}.\n")
-
+print(f"This is the end of the quiz!! Your score id {total_score} out of {total_question}.")
 
 
     
